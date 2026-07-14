426c0e51869233ce215df7a6de4a84a928af7de8
Dharshini V T <vtdharshini8@gmail.com>
2026-07-14 11:10:34 +0530

    Create 2549-CountDistinctNumbersonBoard

---
+++ 2549-CountDistinctNumbersonBoard/version/java/2549-CountDistinctNumbersonBoard_20260714_111036.java
@@ -0,0 +1,9 @@
+// Last updated: 7/14/2026, 11:10:36 AM
+1class Solution {
+2    public int distinctIntegers(int n) {
+3        if(n==1){
+4            return 1;
+5        }
+6        return n-1;
+7    }
+8}
