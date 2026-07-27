8c1bdd514c8ac7c25d4fc7b6cf339a37b6539802
Dharshini V T <vtdharshini8@gmail.com>
2026-07-27 14:42:39 +0530

    Create 1704-DetermineifStringHalvesAreAlike

---
+++ 1704-DetermineifStringHalvesAreAlike/version/java/1704-DetermineifStringHalvesAreAlike_20260727_144248.java
@@ -0,0 +1,19 @@
+// Last updated: 7/27/2026, 2:42:48 PM
+1class Solution {
+2    public boolean halvesAreAlike(String s) {
+3        String vowels = "aeiouAEIOU";
+4        int count = 0;
+5
+6        for (int i = 0; i < s.length() / 2; i++) {
+7            if (vowels.indexOf(s.charAt(i)) != -1)
+8                count++;
+9        }
+10
+11        for (int i = s.length() / 2; i < s.length(); i++) {
+12            if (vowels.indexOf(s.charAt(i)) != -1)
+13                count--;
+14        }
+15
+16        return count == 0;
+17    }
+18}
