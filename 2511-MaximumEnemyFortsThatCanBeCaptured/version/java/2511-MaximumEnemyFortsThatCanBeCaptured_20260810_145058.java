b1d9d1c887b0fc84d20489a58e8ab66a887805b1
Dharshini V T <vtdharshini8@gmail.com>
2026-08-10 14:50:58 +0530

    Create 2511-MaximumEnemyFortsThatCanBeCaptured

---
+++ 2511-MaximumEnemyFortsThatCanBeCaptured/version/java/2511-MaximumEnemyFortsThatCanBeCaptured_20260810_145058.java
@@ -0,0 +1,36 @@
+// Last updated: 8/10/2026, 2:50:58 PM
+1class Solution {
+2    public int captureForts(int[] forts) {
+3        int ans = 0;
+4
+5        for (int i = 0; i < forts.length; i++) {
+6
+7            if (forts[i] == 1) {
+8
+9            
+10                int j = i + 1;
+11
+12                while (j < forts.length && forts[j] == 0) {
+13                    j++;
+14                }
+15
+16                if (j < forts.length && forts[j] == -1) {
+17                    ans = Math.max(ans, j - i - 1);
+18                }
+19
+20            
+21                j = i - 1;
+22
+23                while (j >= 0 && forts[j] == 0) {
+24                    j--;
+25                }
+26
+27                if (j >= 0 && forts[j] == -1) {
+28                    ans = Math.max(ans, i - j - 1);
+29                }
+30            }
+31        }
+32
+33        return ans;
+34    }
+35}
