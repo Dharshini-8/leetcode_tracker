07681b5b08b922e379b661c9840cac80831d765c
Dharshini V T <vtdharshini8@gmail.com>
2026-09-02 09:26:53 +0530

    Create 3875-ConstructUniformParityArrayI

---
+++ 3875-ConstructUniformParityArrayI/version/java/3875-ConstructUniformParityArrayI_20260902_092658.java
@@ -0,0 +1,16 @@
+// Last updated: 9/2/2026, 9:26:58 AM
+1class Solution {
+2    public boolean uniformArray(int[] nums1) {
+3        boolean odd = false;
+4        boolean even = false;
+5
+6        for (int num : nums1) {
+7            if (num % 2 == 0)
+8                even = true;
+9            else
+10                odd = true;
+11        }
+12
+13        return true;
+14    }
+15}
