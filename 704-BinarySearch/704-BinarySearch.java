2eb7d210903244a3736981f8a7869735fe3943c5
Dharshini V T <vtdharshini8@gmail.com>
2026-09-12 14:18:39 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.java
@@ -0,0 +1,26 @@
+// Last updated: 9/12/2026, 2:18:38 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
