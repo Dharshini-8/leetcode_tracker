f8e5cc096be5bc90da2b0c53d2be13b8506f8393
Dharshini V T <vtdharshini8@gmail.com>
2026-09-12 14:16:33 +0530

    Create 2095-DeleteTheMiddleNodeOfALinkedList

---
+++ 2095-DeleteTheMiddleNodeOfALinkedList/2095-DeleteTheMiddleNodeOfALinkedList.java
@@ -0,0 +1,22 @@
+// Last updated: 9/12/2026, 2:16:32 PM
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+}
