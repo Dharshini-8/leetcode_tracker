b286c0d125c063a823cc7f896621489fda306bb5
Dharshini V T <vtdharshini8@gmail.com>
2026-09-12 14:17:37 +0530

    Create 1480-RunningSumOf1dArray

---
+++ 1480-RunningSumOf1dArray/1480-RunningSumOf1dArray.java
@@ -0,0 +1,10 @@
+// Last updated: 9/12/2026, 2:17:36 PM
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i]=nums[i]+nums[i-1];
+        
+    }
+    return nums;
+}
+}
